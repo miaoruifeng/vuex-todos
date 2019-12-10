cdae6c1c39e1aaf3ca5d1f3e5aa02ba0a99c57de
miaoruifeng <mrfnet@163.com>
2019-12-10 11:40:59 +0800

    完成TODO全部功能

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="handleInputChange" />
     <a-button type="primary" @click="addItem">添加事项</a-button>
 
-    <a-list bordered :dataSource="list" class="dt_list">
+    <a-list bordered :dataSource="infolist" class="dt_list">
       <a-list-item slot="renderItem" slot-scope="item">
         <!-- 复选框 -->
-        <a-checkbox>{{item.info}}</a-checkbox>
+        <a-checkbox :checked="item.done" @change="(e) => {ckStateChange(e, item.id)}">{{item.info}}</a-checkbox>
         <!-- 删除链接 -->
         <a slot="actions" @click="removeItemById(item.id)">删除</a>
       </a-list-item>
@@ -14,22 +14,22 @@
       <!-- footer区域 -->
       <div slot="footer" class="footer">
         <!-- 未完成的任务个数 -->
-        <span>0条剩余</span>
+        <span>{{unDoneLength}}条剩余</span>
         <!-- 操作按钮 -->
         <a-button-group>
-          <a-button type="primary">全部</a-button>
-          <a-button>未完成</a-button>
-          <a-button>已完成</a-button>
+          <a-button :type="viewKey === 'all' ? 'primary' : 'default'" @click="changeList('all')">全部</a-button>
+          <a-button :type="viewKey === 'undone' ? 'primary' : 'default'" @click="changeList('undone')">未完成</a-button>
+          <a-button :type="viewKey === 'done' ? 'primary' : 'default'" @click="changeList('done')">已完成</a-button>
         </a-button-group>
         <!-- 把已经完成的任务清空 -->
-        <a>清除已完成</a>
+        <a @click="removeDone">清除已完成</a>
       </div>
     </a-list>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 
 export default {
   name: 'app',
@@ -37,7 +37,8 @@ export default {
     return {}
   },
   computed: {
-    ...mapState(['list', 'inputValue'])
+    ...mapState(['inputValue', 'viewKey']),
+    ...mapGetters(['unDoneLength', 'infolist'])
   },
   methods: {
     // ...mapMutations(['setIpuntValue']),
@@ -57,6 +58,25 @@ export default {
     removeItemById(id) {
       // console.log(id)
       this.$store.commit('removeItem', id)
+    },
+    // 监听复选框 选中状态改变
+    ckStateChange(e, id) {
+      // console.log(e.target.checked)
+      // console.log(id)
+      const param = {
+        id: id,
+        status: e.target.checked
+      }
+      this.$store.commit('changeStatus', param)
+    },
+    // 清除已完成
+    removeDone() {
+      this.$store.commit('removeDone')
+    },
+    // 点击 tab 按钮切换列表数据
+    changeList(key) {
+      // console.log(key)
+      this.$store.commit('changeList', key)
     }
   },
   created() {
--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,8 @@ export default new Vuex.Store({
     // 给 input 绑定数据
     inputValue: '',
     // 默认新增 item 项的 id
-    nextId: 5
+    nextId: 5,
+    viewKey: 'all'
   },
   mutations: {
     initList(state, list) {
@@ -34,10 +35,25 @@ export default new Vuex.Store({
     },
     // 根据 ID 删除对应的 item 项
     removeItem(state, id) {
-      const index = state.list.findIndex(x => x.id === id)
-      if (index !== -1) {
-        state.list.splice(index, 1)
+      const i = state.list.findIndex(x => x.id === id)
+      if (i !== -1) {
+        state.list.splice(i, 1)
       }
+    },
+    // 点击复选框 切换选中状态
+    changeStatus(state, param) {
+      const i = state.list.findIndex(x => x.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    // 清除已完成
+    removeDone(state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    // 点击 tab 按钮切换列表数据
+    changeList(state, key) {
+      state.viewKey = key
     }
   },
   actions: {
@@ -49,6 +65,25 @@ export default new Vuex.Store({
         // console.log(data)
         context.commit('initList', data)
       })
+    }
+  },
+  getters: {
+    // 获取未完成项的数目
+    unDoneLength(state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    // 切换列表数据 根据当前选中的按钮展示不同的列表数据
+    infolist(state) {
+      if (state.viewKey === 'all') {
+        return state.list
+      }
+      if (state.viewKey === 'undone') {
+        return state.list.filter(x => x.done === false)
+      }
+      if (state.viewKey === 'done') {
+        return state.list.filter(x => x.done === true)
+      }
+      return state.list
     }
   },
   modules: {}
